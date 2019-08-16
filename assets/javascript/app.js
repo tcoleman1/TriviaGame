17a3f5428a550cb951e884a9e59af6f5bb0db3f5
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-08-15 22:20:15 -0400

    WIP

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+// document ready 
+
+
+
+function buttonClick (){
+
+    $("#btnSubmit").on("click", function(){
+
+        console.log("You clicked me");
+
+    });
+}
+
+setTimeout(buttonClick, 15000); // this will only alert you've clicked me after 15 seconds....hmmm 
+// I want my set time out func to be related to the timer() ?????????????
+
+buttonClick();
+
+
+// var timer = new 
+
+// var x = setInterval(function () {
+
+    
+// })
